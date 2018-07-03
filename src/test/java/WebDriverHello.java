57746349d316aa536c02a3039bbe98d707eda1d7
Alina_Blazhko <alinablazhko420@gmail.com>
2018-07-03 18:51:44 +0300

    add new steps

--- src/test/java/WebDriverHello.java
+++ src/test/java/WebDriverHello.java
@@ -5,8 +5,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -29,14 +31,11 @@ public class WebDriverHello {
 
     @BeforeClass(description = "Init browser")
     public void beforeClass() {
-        System.setProperty("webdriver.chrome.driver", "c:\\Users\\X240\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("start-maximize");
         driver = new ChromeDriver(options);
         driver.get(URL);
         driver.manage().window().maximize();
-
-
     }
 
 
@@ -62,22 +61,23 @@ public class WebDriverHello {
         assertTrue(driver.getCurrentUrl().equals("https://mail.yandex.ru/?uid=670590425&login=alinablazhko#inbox"));
     }
 
-    @Test(description = "Delete all old emails from draft folder", dependsOnMethods = "loginTest")
-    public void deleteOldEmailsFromDraft(){
+    @Test(description = "Make sure that Draft folder is empty", dependsOnMethods = "loginTest")
+    public void makeSureThatDraftFolderIsEmpty(){
         //Delete all old drafts
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement draftButton = driver.findElement(By.xpath("//span[text()='Черновики']"));
         draftButton.click();
-//        new WebDriverWait(driver, 10)
-//                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.checkbox_view")));
-        WebElement selectAll = driver.findElement(By.cssSelector("span.checkbox_view"));
-        selectAll.click();
-
+        List<WebElement> selectAll = driver.findElements(By.cssSelector("span._nb-checkbox-flag._nb-checkbox-normal-flag"));
+        if (selectAll.size() != 0){
+        for (WebElement element: selectAll){
+            element.click();
+        }}
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement deleteButton = draftButton.findElement(By.xpath("//span[text()='Удалить']"));
         deleteButton.click();
-
     }
 
-    @Test(description = "Create a new mail (fill addressee, subject and body fields) and save as draft", dependsOnMethods = "deleteOldEmailsFromDraft")
+    @Test(description = "Create a new mail (fill addressee, subject and body fields) and save as draft", dependsOnMethods = "loginTest")
     public void writingNewEmailTest(){
         // open new email
         WebElement emailButton = driver.findElement(By.cssSelector("a.mail-ComposeButton.js-left-col-toolbar-compose"));
@@ -92,7 +92,7 @@ public class WebDriverHello {
         WebElement subject = driver.findElement(By.name("subj-e1ac2f662289f670556992795b2809a13640f7ba"));
         subject.sendKeys(SUBJECT);
         // Type email's text
-        WebElement body = driver.findElement(By.cssSelector("div.cke_wysiwyg_div.cke_reset.cke_enable_context_menu.cke_editable.cke_editable_themed.cke_contents_ltr.cke_show_borders"));
+        WebElement body = driver.findElement(By.cssSelector("textarea.cke_source.cke_reset.cke_enable_context_menu.cke_editable.cke_editable_themed.cke_contents_ltr"));
         body.sendKeys(BODY);
 
 
@@ -106,35 +106,33 @@ public class WebDriverHello {
         WebElement closeButton = driver.findElement(By.cssSelector("button.nb-button._nb-small-action-button._init.nb-with-s-right-gap.js-resolve"));
         closeButton.click();
 
-        while(driver.findElements(By.xpath(".//span[text()='Email for test']")).size() == 0 )
-            driver.findElement(By.xpath("//body")).sendKeys(Keys.F5);
+        WebElement refreshListButton = driver.findElement(By.cssSelector("span.mail-ComposeButton-Refresh.js-left-col-toolbar-refresh.ns-action"));
+        refreshListButton.click();
 
-        WebElement draftEmail = driver.findElement(By.xpath(".//span[text()='Email for test']"));
-        assertTrue(draftEmail.isDisplayed());
+        List<WebElement> draftEmail = driver.findElements(By.cssSelector("span.mail-MessageSnippet-Item.mail-MessageSnippet-Item_body.js-message-snippet-body"));
+        assertTrue(draftEmail.size() == 1);
 
-        draftEmail.click();
+        draftEmail.get(0).click();
 
-        new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='mail-Bubble-Block']/span[text()='alinaBlazhko@yandex.ru']")));
+//        new WebDriverWait(driver, 10)
+//                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='mail-Bubble-Block']/span[text()='alinaBlazhko@yandex.ru']")));
 
         WebElement recipient = driver.findElement(By.xpath("//span[@class='mail-Bubble-Block']/span[text()='alinaBlazhko@yandex.ru']"));
-        assertTrue(recipient.isDisplayed());
+        assertTrue(recipient.equals(USER));
 
         WebElement sub = driver.findElement(By.xpath("//input[@value='Email for test']"));
-        assertTrue(sub.isDisplayed());
+        assertTrue(sub.equals(SUBJECT));
 
         WebElement text = driver.findElement(By.xpath("//div[text()='Hello Mr. Smith!']"));
-        assertTrue(text.isDisplayed());
+        assertTrue(text.equals(BODY));
 
         WebElement sendButton = driver.findElement(By.xpath("//span[text()='Отправить']"));
         sendButton.click();
-
-
     }
-//
-//    @AfterClass
-//    public void afterClass(){
-//        driver.quit();
-//    }
+
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
 
 }
