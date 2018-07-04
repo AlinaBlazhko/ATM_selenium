ec87b200a65847ac8e48759b8ff44348a5fd0b56
AlinaBlazhko <alinablazhko420@gmail.com>
2018-07-04 10:23:26 +0300

    add new steps

--- src/test/java/CommonTest.java
+++ src/test/java/CommonTest.java
@@ -1,17 +1,25 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-imp
+
+import java.util.concurrent.TimeUnit;
+
+
+import java.util.*;
 
 //import java.awt.*;
 
+import static com.sun.org.apache.xml.internal.serialize.LineSeparator.Web;
 import static org.testng.Assert.assertTrue;
 
 /**
@@ -34,6 +42,7 @@ public class CommonTest {
         driver = new ChromeDriver(options);
         driver.get(URL);
         driver.manage().window().maximize();
+
     }
 
 
@@ -58,10 +67,46 @@ public class CommonTest {
                 .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.mail-User-Name")));
 
         assertTrue((driver.getCurrentUrl()).equals("https://mail.yandex.ru/?uid=670590425&login=alinablazhko#inbox"));
+
+        //Make sure that draft folder is empty
+        // refresh draft folder
+        WebElement draftButton = driver.findElement(By.xpath("//span[text() = 'Черновики']"));
+        draftButton.click();
+
+        // refresh draft folder
+//        new WebDriverWait(driver, 20)
+//                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@title='Проверить, есть ли новые письма (F9)']")));
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//        WebElement refreshButton = driver.findElement(By.xpath("//span[@title='Проверить, есть ли новые письма (F9)']"));
+        Boolean until = new WebDriverWait( driver, 30 ).until( new ExpectedCondition<Boolean>()
+        {
+            public Boolean apply( WebDriver webDriver )
+            {
+                try
+                {
+                    driver.findElement(By.xpath("//span[@title='Проверить, есть ли новые письма (F9)']")).click();
+                }
+                catch( StaleElementReferenceException e )
+                {
+                    System.out.println( "Select failed! Try again..." );
+                    return false;
+                }
+                System.out.println( "test found!" );
+                return true;
+            }
+        } );
+
+        List<WebElement> check = driver.findElements(By.cssSelector("span._nb-checkbox-flag._nb-checkbox-normal-flag"));
+        for (WebElement element : check) {
+            element.click();
+        }
+        WebElement deleteButton = driver.findElement(By.xpath("//span[text() = 'Удалить']"));
+        deleteButton.click();
     }
 
     @Test(dependsOnMethods = "loginTest")
-    public void createEmailAndSaveAsDraft(){
+    public void createEmailAndSaveAsDraft() {
         //open new email
         WebElement createEmailButton = driver.findElement(By.cssSelector("span.mail-ComposeButton-Text"));
         createEmailButton.click();
@@ -97,15 +142,22 @@ public class CommonTest {
         WebElement refreshButton = driver.findElement(By.cssSelector("span.mail-ComposeButton-Refresh.js-left-col-toolbar-refresh.ns-action"));
         refreshButton.click();
 
-        List<WebElement> check = driver.findElements(By.cssSelector("input.checkbox_controller"));
-        for(Web)
-        check.click();
+        new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.mail-MessageSnippet-Item.mail-MessageSnippet-Item_left.js-message-snippet-left")));
 
+//        WebElement email = driver.findElement(By.className("span.mail-MessageSnippet-Item.mail-MessageSnippet-Item_left.js-message-snippet-left"));
+//        Assert.assertTrue(email.isDisplayed());
+
+        List<WebElement> check = driver.findElements(By.cssSelector("span._nb-checkbox-flag._nb-checkbox-normal-flag"));
+        Assert.assertTrue(check.size() == 1);
+
+//        WebElement email = driver.findElement(By.className("span.mail-MessageSnippet-Item.mail-MessageSnippet-Item_left.js-message-snippet-left"));
+//        email.click();
 
     }
 
-//    @AfterClass
-//    public void afterClass(){
-//        driver.quit();
-//    }
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
 }
