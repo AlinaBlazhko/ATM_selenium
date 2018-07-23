661dda8625f34c9245602b13388c19b52de1aaf9
AlinaBlazhko <alinablazhko420@gmail.com>
2018-07-23 08:25:11 +0300

    add page object pattern

---
+++ src/test/java/po/FoldersPage.java
@@ -0,0 +1,12 @@
+package po;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by X240 on 7/22/2018.
+ */
+public class FoldersPage extends AbstractPage{
+    public FoldersPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/test/java/po/LoginPage.java
@@ -0,0 +1,34 @@
+package po;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by X240 on 7/22/2018.
+ */
+public class LoginPage extends AbstractPage{
+
+    private static final String USER = "alinaBlazhko@yandex.ru";
+    private static final String PASSWORD = "gfhjkmkzntcnf";
+
+    private By userName = By.name("login");
+    private By password = By.name("passwd");
+    private By loginButton = By.xpath("//span[text()='Войти']");
+
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void login(){
+        waitForElementVisibility(userName);
+        driver.findElement(userName).sendKeys(USER);
+        driver.findElement(password).sendKeys(PASSWORD);
+        driver.findElement(loginButton).click();
+        waitForElementVisibility(By.xpath("//div[text()='alinaBlazhko']"));
+    }
+
+}
