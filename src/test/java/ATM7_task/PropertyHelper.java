8a4214f156924f54e0c31070bf951f7de70abdd8
AlinaBlazhko <alinablazhko420@gmail.com>
2018-08-11 20:01:21 +0300

    add config file for grid settings and write PropertyHelper

---
+++ src/test/java/ATM7_task/PropertyHelper.java
@@ -0,0 +1,27 @@
+package ATM7_task;
+
+import java.io.*;
+import java.util.Properties;
+
+
+/**
+ * Created by X240 on 8/11/2018.
+ */
+public class PropertyHelper {
+    public static String getGridHost(){
+        FileInputStream fis;
+        Properties property = new Properties();
+
+        String host = null;
+
+        try {
+            fis = new FileInputStream("src/test/resources/config.properties");
+            property.load(fis);
+            host = property.getProperty("grid.localhost");
+            System.out.println("HOST: " + host);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return host;
+    }
+}
